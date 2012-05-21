8959dc1b00cd33b186194fd2557545f42ba61c9d
fjgarcia <fjgarcia@106-24.info2>
2012-05-21 13:02:13 +0200

    Primer commit

---
+++ fjgarcia/src/clase2.java
@@ -0,0 +1,13 @@
+
+public class clase2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Soy la clase 1");
+
+	}
+
+}
